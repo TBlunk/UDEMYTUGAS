ba82cac2f30ba576bf78600e232194385e2c62ca
RAN <126699689+TBlunk@users.noreply.github.com>
2023-09-26 09:33:17 +0700

    Add files via upload

---
+++ UDEMY 2.0/Guess Number/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 
+// 'Correct Number!';
+
+// document.querySelector('.number') = 13;
+// document.querySelector('.score') = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const x = function() {
+    console.log(23);
+}
+
+document.querySelector('.check').addEventListener('click', function() {
+console.log(document.querySelector('.guess').value);
+});
